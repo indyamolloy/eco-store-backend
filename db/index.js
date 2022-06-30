f93f5214d127db87d44ef0626fe28753ee5abd01
Indya 2 <indyacmolloy@gmail.com>
2022-06-30 11:57:17 +0100

    connection string added

--- db/index.js
+++ db/index.js
@@ -2,15 +2,9 @@ import pg from "pg";
 import { db } from "../config.js";
 
 const pool = new pg.Pool({
-  user: db.user,
-  host: db.host,
-  database: db.database,
-  password: db.password,
-  port: db.port,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
-console.log(db);
 
 export default (text, params) => {
   return pool.query(text, params);
